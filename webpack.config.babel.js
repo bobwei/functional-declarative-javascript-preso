57a5dd5414f38981ca6adbfe29ac9313504bc409
Bob Wei <bob.wei@linecorp.com>
2017-11-14 00:14:13 +0800

    Support hot reload for index.html

---
+++ webpack.config.babel.js
@@ -0,0 +1,60 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+const config = {
+  entry: [
+    ...(process.env.NODE_ENV === 'production'
+      ? []
+      : ['webpack-dev-server/client?http://localhost:8080']),
+    './src/index.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new CopyWebpackPlugin([{ from: 'src' }]),
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+              warnings: false,
+            },
+          }),
+          new webpack.optimize.OccurrenceOrderPlugin(),
+        ]
+      : [
+          new webpack.HotModuleReplacementPlugin(),
+          new HtmlWebpackPlugin({
+            template: './src/index.html',
+          }),
+        ]),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'raw-loader',
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+    historyApiFallback: true,
+    hot: true,
+    noInfo: false,
+    quiet: false,
+    lazy: false,
+    stats: {
+      cached: false,
+      chunks: false,
+    },
+  },
+};
+
+export default config;
