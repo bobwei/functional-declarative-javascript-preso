5726473fcd55b7f770d2860cc6d1d74499c41ae9
Bob Wei <bob.wei@linecorp.com>
2017-11-16 16:06:11 +0800

    Add on menu click

--- src/modules/presentation/index.js
+++ src/modules/presentation/index.js
@@ -1,3 +1,13 @@
+/* eslint-disable global-require */
+/* global document */
+import R from 'ramda';
+import $ from 'jquery';
+
 if (process.env.NODE_ENV !== 'production') {
   require('./index.html');
 }
+
+$(document).on(
+  'keypress',
+  R.cond([[R.propEq('keyCode', 109), () => console.log('on menu click')]]),
+);
