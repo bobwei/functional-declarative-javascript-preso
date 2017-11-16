44f0ee3aff8ce3664421d9c96539762b7d6da933
Bob Wei <bob.wei@linecorp.com>
2017-11-16 17:27:34 +0800

    Updates

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('./index.html');
-}
+/* global document, Reveal */
+import R from 'ramda';
+import $ from 'jquery';
+import './dev';
+
+$(document).on(
+  'keypress',
+  R.cond([
+    [
+      R.propEq('keyCode', 109),
+      () => {
+        /* toc page */
+        Reveal.slide(1);
+      },
+    ],
+  ]),
+);
